03a6b4e6b7ac2d23d7c2dd537302016721b8826e
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-15 09:37:06 +0200

    editing  files and folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App">hello</div>;
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
