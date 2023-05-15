d79258dccd45f3356d21df9016602cabef82e2fd
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-16 00:47:03 +0200

    update

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Group from "../../images/group.jpg";
 
 const Index = () => {
-  return;
+  return (
+    <>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+          autem dignissimos, repellendus nihil fugit iure eaque culpa? Neque
+          quidem incidunt repellendus sit, dolores doloribus, aliquid saepe
+          commodi itaque nesciunt doloremque.
+        </p>
+        <img src={Group} alt="img " />
+      </div>
+    </>
+  );
 };
 
 export default Index;
