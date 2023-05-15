462e4133d684626e017f10a2ebb34d857aa8b7f9
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-15 11:13:56 +0200

    adding theme variabale

--- src/style/websiteStyles/GlobalStyle.jsx
+++ src/style/websiteStyles/GlobalStyle.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = () => {
-  return <div>GlobalStyle</div>;
-};
+const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+}
+html{
+    --color-primary:#1000CB;
+    --color-secondary:#FFE4E0;
+    --color-text: #484747;
+    --html-bgcolor :#FAFAFA
+    height: 100%;
+}
+body{
+    height: 100%;
+}
 
+`;
 export default GlobalStyle;
+// buttonBorder: "#FFE4E0",
+
+// htmlBGcolor: "#FAFAFA",
+// tertiary: "#FF0000",
+// saleText: "#ffffff",
