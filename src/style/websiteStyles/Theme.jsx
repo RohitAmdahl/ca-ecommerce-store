40ba6cbc1667f012db63878fed80d20901be8bc5
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-15 11:13:37 +0200

    adding theme variable

--- src/style/websiteStyles/Theme.jsx
+++ src/style/websiteStyles/Theme.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
-const Theme = () => {
-  return <div>Theme</div>;
+const theme = {
+  color: {
+    buttonborder: "#FFE4E0",
+    text: "var(--color-text)",
+    htmlbgcolor: "var(--html-bgcolor)",
+    primary: "var(--color-primary)",
+    secondary: "var(--color-secondary)",
+    tertiary: "#FF0000",
+    saleText: "#ffffff",
+    media: { mobile: "420px", tab: "998px", laptop: "1200" },
+  },
 };
+
+export const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
 
 export default Theme;
