6c54f14d2a60da7e714de91085bd8622a1eba493
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-16 00:44:08 +0200

    updating file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Index from "../components/layout/Index";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Index />;
+    </>
   );
 };
 
