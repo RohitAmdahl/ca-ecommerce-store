0f69a98c6dd8a25873f1a558428a9a109429aef7
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-19 23:36:30 +0200

    404 error page done

--- src/pages/Detailpage.jsx
+++ src/pages/Detailpage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+// import { useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
 
 const Detailpage = () => {
-  return <div>Detailpage</div>;
+  return <div>hello</div>;
 };
 
 export default Detailpage;
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ErrorPage from "../components/layout/Error/ErrorPage";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <>
+      <ErrorPage />
+    </>
+  );
 };
 
 export default PageNotFound;
