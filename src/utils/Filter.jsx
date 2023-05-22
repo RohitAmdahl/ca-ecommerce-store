e6a51fc5ec94dd597f391c6701c82e5d0dd1c7c3
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-22 14:40:23 +0200

    edit code

--- src/utils/Filter.jsx
+++ src/utils/Filter.jsx
@@ -1,6 +1,6 @@
-// import React, { useContext } from "react";
+//  import React, { useContext } from "react";
 
-// const filterProducts = products.filter((items) => {
-//   return items.tags[0] === "electronics" || items.tags[0] === "shoes";
-// });
-// console.log(filterProducts);
+//  const filterProducts = products.filter((items) => {
+//    return items.tags[0] === "electronics" || items.tags[0] === "shoes";
+//  });
+//  console.log(filterProducts);
